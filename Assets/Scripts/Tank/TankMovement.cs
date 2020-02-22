95e335466f4e9e6681bab5c7f5876dd5008783fd
Ivan Mollov <ivan.xero.mollov@gmail.com>
2020-02-22 02:18:11 +0200

    Adds touch input
    - Reading inputs in separate methods
    - Added support for touch input
    - Methods ordering

--- Assets/Scripts/Tank/TankMovement.cs
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -2,20 +2,30 @@
 
 public class TankMovement : MonoBehaviour
 {
-	public readonly float speed = 12f;
-	public readonly float turnSpeed = 180f;
+	public readonly float speedPerFrame = 12f;
 
 	public int playerNumber = 1;
+
 	public AudioSource movementAudio;
 	public AudioClip engineIdling;
 	public AudioClip engineDriving;
 
 
+	private new Rigidbody rigidbody;
+
+#if UNITY_ANDROID || UNITY_IOS
+	private Vector2 touchStartPosition;
+	private Vector2 movementDirection;
+#elif UNITY_WEBPLAYER || UNITY_STANDALONE
+	public readonly float maxSpeed = 1f;
+	public readonly float turnSpeed = 180f;
+
 	private string movementAxisName;
 	private string turnAxisName;
-	private new Rigidbody rigidbody;
+
 	private float movementInputValue;
 	private float turnInputValue;
+#endif
 
 
 	private void Awake() => this.rigidbody = this.GetComponent<Rigidbody>();
@@ -23,41 +33,26 @@ public class TankMovement : MonoBehaviour
 
 	private void OnEnable() {
 		this.rigidbody.isKinematic = false;
-		this.movementInputValue = 0f;
-		this.turnInputValue = 0f;
-	}
-
-
-	private void Start() {
-		this.movementAxisName = "Vertical" + this.playerNumber;
-		this.turnAxisName = "Horizontal" + this.playerNumber;
+		
+		this.ResetInput();
 	}
 
 
 	private void OnDisable() => this.rigidbody.isKinematic = true;
 
+
 	private void Update() {
 		// Store the player's input and make sure the audio for the engine is playing.
 
-		this.movementInputValue = Input.GetAxis(this.movementAxisName);
-		this.turnInputValue = Input.GetAxis(this.turnAxisName);
-
+		this.GetInput();
 		this.UpdateEngineAudio();
-	}
-
-
-	private void FixedUpdate() {
-		// Move and turn the tank.
-
-		this.Move();
-		this.Turn();
 	}
 
 
 	private void UpdateEngineAudio() {
 		// Play the correct audio clip based on whether or not the tank is moving and what audio is currently playing.
 
-		if (Mathf.Abs(this.movementInputValue) > .1f || Mathf.Abs(this.turnInputValue) > .1f) {
+		if (this.IsMoving()) {
 			if (this.movementAudio.clip == this.engineIdling) {
 				this.movementAudio.clip = this.engineDriving;
 				this.movementAudio.Play();
@@ -72,9 +67,90 @@ public class TankMovement : MonoBehaviour
 	}
 
 
+#if UNITY_ANDROID || UNITY_IOS
+	private void FixedUpdate() {
+		if (this.IsMoving()) {
+			float distancePerFrame = this.speedPerFrame * Time.deltaTime;
+			Vector3 movement = new Vector3(this.movementDirection.x, 0f, this.movementDirection.y) * distancePerFrame;
+			this.rigidbody.MovePosition(this.rigidbody.position + movement);
+
+			float angleFromStraight = Vector2.SignedAngle(this.movementDirection, Vector2.up);
+			this.rigidbody.MoveRotation(Quaternion.Euler(0f, angleFromStraight, 0f));
+		}
+	}
+
+
+	private void ResetInput() {
+		this.touchStartPosition = Vector2.zero;
+		this.movementDirection = Vector2.zero;
+	}
+
+
+	private void GetInput() {
+		Touch? touchInput = this.GetTouch();
+
+		if (touchInput.HasValue) {
+			Touch touch = touchInput.Value;
+
+			if (touch.phase == TouchPhase.Began)
+				this.touchStartPosition = touch.position;
+			else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+				this.movementDirection = (touch.position - this.touchStartPosition).normalized;
+			else if (touch.phase == TouchPhase.Ended)
+				this.ResetInput();
+			else
+				Debug.Assert(touch.phase == TouchPhase.Canceled);
+		}
+		else {
+			Debug.Assert(!touchInput.HasValue);
+
+			this.ResetInput();
+		}
+	}
+
+
+	private Touch? GetTouch() {
+		for (int i = 0; i < Input.touchCount; ++i) {
+			Touch touch = Input.GetTouch(i);
+
+			if (touch.position.x < Screen.width / 2)
+				return touch;
+		}
+
+		return null;
+	}
+
+
+	private bool IsMoving() => this.movementDirection.magnitude > 0f;
+
+#elif UNITY_WEBPLAYER || UNITY_STANDALONE
+	private void Start() {
+		this.movementAxisName = "Vertical" + this.playerNumber;
+		this.turnAxisName = "Horizontal" + this.playerNumber;
+	}
+
+
+	private void FixedUpdate() {
+		this.Move();
+		this.Turn();
+	}
+
+
+	private void ResetInput() {
+		this.movementInputValue = 0f;
+		this.turnInputValue = 0f;
+	}
+
+
+	private void GetInput() {
+		this.movementInputValue = Input.GetAxis(this.movementAxisName);
+		this.turnInputValue = Input.GetAxis(this.turnAxisName);
+	}
+
+
 	private void Move() {
 		// Adjust the position of the tank based on the player's input.
-
+	
 		float distancePerFrame = this.movementInputValue * this.speed * Time.deltaTime;
 		Vector3 movement = this.transform.forward * distancePerFrame;
 
@@ -84,9 +160,15 @@ public class TankMovement : MonoBehaviour
 
 	private void Turn() {
 		// Adjust the rotation of the tank based on the player's input.
+	
 		float rotationPerFrame = this.turnInputValue * this.turnSpeed * Time.deltaTime;
 		var turn = Quaternion.Euler(0f, rotationPerFrame, 0f);
 
 		this.rigidbody.MoveRotation(this.rigidbody.rotation * turn);
 	}
+
+
+	private bool IsMoving() => Mathf.Abs(this.movementInputValue) > .1f || Mathf.Abs(this.turnInputValue) > .1f;
+
+#endif
 }
--- Assets/Scripts/Tank/TankShooting.cs
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -21,16 +21,16 @@ public class TankShooting : MonoBehaviour
 	private bool fired;
 
 
-	private void OnEnable() {
-		this.currentLaunchForce = this.minLaunchForce;
-		this.aimSlider.value = this.minLaunchForce;
-	}
-
-
 	private void Start() {
 		this.fireButton = "Fire" + this.playerNumber;
 
 		this.chargeSpeed = (this.maxLaunchForce - this.minLaunchForce) / this.maxChargeTime;
+	}
+
+
+	private void OnEnable() {
+		this.currentLaunchForce = this.minLaunchForce;
+		this.aimSlider.value = this.minLaunchForce;
 	}
 
 
@@ -43,18 +43,18 @@ public class TankShooting : MonoBehaviour
 			this.currentLaunchForce = this.maxLaunchForce;
 			this.Fire();
 		}
-		else if (Input.GetButtonDown(this.fireButton)) {
+		else if (this.InputBeginShot()) {
 			this.fired = false;
 			this.currentLaunchForce = this.minLaunchForce;
 
 			this.shootingAudio.clip = this.chargingClip;
 			this.shootingAudio.Play();
 		}
-		else if (Input.GetButton(this.fireButton) && !this.fired) {
+		else if (this.InputChargingShot() && !this.fired) {
 			this.currentLaunchForce += this.chargeSpeed * Time.deltaTime;
 			this.aimSlider.value = this.currentLaunchForce;
 		}
-		else if (Input.GetButtonUp(this.fireButton) && !this.fired) {
+		else if (this.InputEndShot() && !this.fired) {
 			this.Fire();
 		}
 	}
@@ -73,4 +73,56 @@ public class TankShooting : MonoBehaviour
 
 		this.currentLaunchForce = this.minLaunchForce;
 	}
+
+
+#if UNITY_ANDROID || UNITY_IOS
+	private bool InputBeginShot() {
+		Touch? touch = this.GetTouch();
+
+		return !touch.HasValue ? false : 
+			touch.Value.phase == TouchPhase.Began;
+	}
+
+
+	private bool InputChargingShot() {
+		Touch? touch = this.GetTouch();
+
+		return !touch.HasValue ? false :
+			touch.Value.phase == TouchPhase.Stationary || touch.Value.phase == TouchPhase.Moved;
+	}
+
+
+	private bool InputEndShot() {
+		Touch? touch = this.GetTouch();
+
+		return !touch.HasValue ? false :
+			touch.Value.phase == TouchPhase.Ended || touch.Value.phase == TouchPhase.Canceled;
+	}
+
+	private Touch? GetTouch() {
+		for (int i = 0; i < Input.touchCount; ++i) {
+			Touch touch = Input.GetTouch(i);
+
+			if (touch.position.x > Screen.width / 2)
+				return touch;
+		}
+
+		return null;
+	}
+#elif UNITY_WEBPLAYER || UNITY_STANDALONE
+	private bool InputBeginShot() {
+		return Input.GetButtonDown(this.fireButton);
+	}
+
+
+	private bool InputChargingShot() {
+		return Input.GetButton(this.fireButton);
+	}
+
+
+	private bool InputEndShot() {
+		return Input.GetButtonUp(this.fireButton);
+	}
+#endif
+
 }

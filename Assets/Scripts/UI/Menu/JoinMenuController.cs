e7e38861edfa1506c774da5d52452758b921fda2
Ivan Mollov <ivan.xero.mollov@gmail.com>
2020-06-29 04:32:08 +0300

    Adds Join Match Menu

--- Assets/Scripts/UI/Menu/JoinMenuController.cs
+++ Assets/Scripts/UI/Menu/JoinMenuController.cs
@@ -1,11 +1,48 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine.UI;
 
 public class JoinMenuController : MonoBehaviour
 {
-	public void JoinButton() {
-		NetworkManager manager = NetworkManager.singleton;
+	private NetworkManager manager;
 
-		_ = manager.matchMaker.CreateMatch(manager.matchName, manager.matchSize, matchAdvertise: true, "", "", "", 0, 0, manager.OnMatchCreate);
+	public Button joinMatchButtonPrefab;
+	public Dictionary<string, Button> matchButtons;
+
+
+	JoinMenuController() {
+		matchButtons = new Dictionary<string, Button>();
+	}
+
+
+	private void Awake() {
+		this.manager = NetworkManager.singleton;
+		this.manager.matchMaker.ListMatches(0, 20, "", filterOutPrivateMatchesFromResults: false, 0, 0, this.OnMatchList);
+	}
+
+	private void Update() {
+	}
+
+	private void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matchList) {
+		this.matchButtons.Clear();
+
+		if (success) {
+			foreach (MatchInfoSnapshot match in matchList) {
+				Button joinMatchButton = Instantiate(this.joinMatchButtonPrefab, parent: this.transform);
+				Text buttonLabel = joinMatchButton.GetComponentInChildren<Text>();
+				buttonLabel.text = "Join " + match.name;
+				joinMatchButton.onClick.AddListener(() => this.JoinButton(match));
+
+				this.matchButtons[match.name] = joinMatchButton;
+			}
+		}
+	}
+
+
+	public void JoinButton(MatchInfoSnapshot match) {
+		this.manager.matchName = match.name;
+		_ = this.manager.matchMaker.JoinMatch(match.networkId, "", "", "", 0, 0, this.manager.OnMatchJoined);
 	}
 }
